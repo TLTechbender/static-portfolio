335df7866f957553fa599e2369825041442c4e6f
TLTechbender <myjoyhasarrived2004@gmail.com>
2023-02-15 23:23:47 +0100

    bugs

---
+++ styling/script.js
@@ -0,0 +1,9 @@
+var navLinks = document.getElementById("nav-buttons");
+function closeMenu(){
+    navLinks.style.left="0px";
+    
+   }
+
+   function openMenu(){
+    navLinks.style.left="0";
+   }
